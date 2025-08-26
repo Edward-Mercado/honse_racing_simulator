4209cb970bebdf5ccb3bb26d1d2d4aa241709a8c
Edward-Mercado <edwardmercado149@gmail.com>
2025-08-26 15:56:41 -0400

    fix import error bc i removed a useless function

--- horse_logic.py
+++ horse_logic.py
@@ -1,5 +1,5 @@
 import pygame, random, math, json
-from collision_logic import handle_horse_collision, handle_wall_collision, get_opposite_direction, get_horse_start_pos
+from collision_logic import handle_horse_collision, handle_wall_collision
 
 class Horse:
     def __init__(self, name, speed, width, height, location_x, location_y, image_url, win_image_url, win_song_url):
